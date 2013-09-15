ddb34bbd5181e03cbc27cde7fec2a5db06933d74
yu0819ki <yu0819ki@gmail.com>
2013-09-16 03:32:16 +0900

    set BaseController to use mustache template. And some settings are in configure file.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,18 +1,45 @@
 <?php
 
-class BaseController extends Controller {
+/**
+ * 基底コントローラ
+ *
+ * @author yu0819ki<yu0819ki@gmail.com>
+ * @package maimai
+ */
+class BaseController extends Controller
+{
+    /** @type array $defaultContents  コンテンツ表示に利用する設定配列 */
+    protected $defaultContents = array();
 
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
+    /** @type string $layout  マスタッシュテンプレートによるレイアウト */
+    public $layout = 'layouts.mustache.master';
+
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if ( ! is_null($this->layout))
+        {
+            $this->layout = View::make($this->layout);
+        }
+    }
+
+    protected function loadDefaultContents($type = null)
+    {
+        $this->defaultContents = Config::get('mustache.defaultContents');
+    }
+
+    protected function setTitle($title)
+    {
+        $this->defaultContents['title'] = $title;
+    }
+
+    protected function setPageSettings($settings)
+    {
+        $this->defaultContents['page'] = array_merge($this->defaultContents['page'], $settings);
+    }
 
 }
